b0cf79250a44ecae1f85aa22c908281e15532027
anamachadocarolina <acarolina066@gmail.com>
2023-12-09 23:20:43 -0300

    Cria estrutura inicial do Router

--- app/application.php
+++ app/application.php
@@ -7,5 +7,7 @@
 require_once('Configs/constantes.php');
 
 require_once(COMPOSER_PATH . '/autoload.php');
+
+require_once('Configs/routers.php');
 // require_once('Core/View.php');
 // require_once('Controllers/Home.php');
--- public/index.php
+++ public/index.php
@@ -2,9 +2,26 @@
 
 require_once('../app/application.php');
 use Controllers\Home;
+use Core\Router;
 
-$controller = new Home();
-$controller->index();
+// $controller = new Home();
+// $controller->index();
+
+$url = '/';
+if(isset($_GET['url'])){
+    $url = $_GET['url'];
+} 
+
+$router = Router::getRouterByUrl($url);
+
+if($router){
+    $controller = $router->getController();
+    $action = $router->getAction();
+    $controller = new $controller();
+    $controller->$action();
+} else {
+    die('page 404');
+}
 
 //echo $_GET['url']; testa se o arquivo .htaccess está funcionando
 /*use Core\View;
