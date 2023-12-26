2f2d2cd1792580335276f8e069aba4fab5a0ab76
anamachadocarolina <acarolina066@gmail.com>
2023-12-26 16:17:45 -0300

    Cria código para Where e Load de registros

--- app/Core/Model.php
+++ app/Core/Model.php
@@ -15,7 +15,15 @@ abstract class Model{
 
     protected $__data = [];
 
+    private $where = [];
+
     protected $pk = 'id';
+
+    public function __construct($id = null){
+        if(isset($id)){
+            $this->load($id);
+        }
+    }
 
     public function __set(string $name, $value){
         if(in_array($name,$this->columns)){
@@ -34,7 +42,16 @@ abstract class Model{
         $stm = $conn->prepare($sql);
         $stm->execute($data);
         return $stm;
-    }    
+    }
+
+    private function load($id){
+        $this->where($this->pk, '=', $id);
+        $stm = $this->select();
+        $result = $stm->fetch(\PDO::FETCH_ASSOC);
+        if($result){
+            $this->__data = $result;
+        }
+    }
     /**
      * Inserção no banco de dados
      * @return int
@@ -69,8 +86,9 @@ abstract class Model{
     
     private function select(){
         $columns = implode(', ',$this->columns);
-        $sql = "SELECT $columns FROM $this->table;";
-        return $this->query($sql);
+        [$where, $data] = $this->flushWhere();
+        $sql = "SELECT $columns FROM $this->table$where;";
+        return $this->query($sql, $data);
     }
 
     public function all(){
@@ -78,5 +96,29 @@ abstract class Model{
     }
     public function get(){
         return $this->select()->fetchObject(get_class($this));
+    }
+
+    public function where($column, $comparison, $value){
+        $this->where[] = ['AND', $column, $comparison, $value];
+        return $this;
+    }
+
+    public function orWhere($column, $comparison, $value){
+        $this->where[] = ['OR', $column, $comparison, $value];
+        return $this;
+    }
+
+    private function flushWhere(){
+        $where = '';
+        $data = [];
+        if(count($this->where)>0){
+            $this->where[0][0] = 'WHERE';
+            foreach($this->where as $w){
+                $where .= " $w[0] $w[1] $w[2] :$w[1]";
+                $data[$w[1]] = $w[3];
+            }
+            $this->where = [];
+        }
+        return [$where, $data];
     }
 }
--- public/teste.php
+++ public/teste.php
@@ -5,9 +5,12 @@ use Models\Produto;
 
 require_once('../app/application.php');
 
-$config = new Config();
-$config->name = 'ENDERECO';
-$config->value = 'Rua 13, nº 515';
-$config->insert(['alteracao_data' => date('Y-m-d H:i:s')]);
-//pre($model->insert(['name'=>'HELP_CENTER','value'=>'999999999']));localhost/phpmyadmin
-pre($config->all());
+$config = new Config(2);
+echo $config->name . " = " . $config->value;
+
+
+
+
+// defined($config->name)  || define($config->name, $config->value);
+
+// echo VERSION;
