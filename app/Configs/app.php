374654dfa2867bc6f8502a4200b6a35a4295820f
anamachadocarolina <acarolina066@gmail.com>
2024-01-01 18:11:25 -0300

    Cria session

--- app/Configs/app.php
+++ app/Configs/app.php
@@ -15,3 +15,5 @@ defined ('APPLICATION_LANGUAGE') || define('APPLICATION_LANGUAGE', 'pt-br');
 defined ('APPLICATION_VERSION') || DEFINE('APPLICATION_VERSION', '1.0.0');
 
 defined ('PAGE_404') || DEFINE('PAGE_404', [\Controllers\ErrorController::class,'page404']);
+
+defined ('SESSION_NAME') || DEFINE('SESSION_NAME', 'Restaurante_IF');
